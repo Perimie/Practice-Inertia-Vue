2e16d44119747cbc8f64fcaf11776f2f56486630
Perimie <114374380+Perimie@users.noreply.github.com>
2025-01-22 11:01:33 +0800

    Mini up

--- resources/js/Pages/Frontend/About.vue
+++ resources/js/Pages/Frontend/About.vue
@@ -1,18 +1,20 @@
+<script setup>
+    
+defineProps({
+title: {
+    type: String,
+},
+
+});
+</script>
+
 <template>
     <h1>
         <div>
+
             {{title}} 
         </div>
     </h1>
     
     </template>
     
-    <script setup>
-    
-    defineProps({
-    title: {
-        type: String,
-    },
-    
-});
-    </script>
